55abf92323af2ae5fb3fe64d8e06bd671685b493
Anderson Santos <anderson.chenko1993@gmail.com>
2026-08-15 12:20:14 -0300

    feat: implementing count signals

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,32 @@
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {
+  protected title = 'Welcome to Modern Angular!';
+  protected isDisabled = false;
+
+  protected onClick() {
+    this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+
+  protected doubleCount = computed(() => this.count() * 2);
+
+  protected increaseCounter() {
+    this.count.update((value) => value + 1);
+  }
+
+  protected decreaseCounter() {
+    this.count.update((value) => value - 1);
+  }
+
+  protected resetCounter() {
+    this.count.set(0);
+  }
+}
