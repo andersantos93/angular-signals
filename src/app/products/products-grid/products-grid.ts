19e45929a618d8286b9335ffd18b2b46fb0b7d5e
Anderson Santos <anderson.chenko1993@gmail.com>
2026-08-16 10:33:51 -0300

    feat: bind product signal to child product card component

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,10 +1,35 @@
 import { Component, signal } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
+import { Product } from '../product';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard],
+  imports: [ProductCard, MatIconModule],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
-export class ProductsGrid {}
+export class ProductsGrid {
+  protected readonly products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Premium Wireless Headphones',
+      description:
+        'High-quality wireless headphones with noise cancellation and long battery life.',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Smart Fitness Watch',
+      description:
+        'Track your fitness goals with this advanced smart watch featuring heart rate monitoring.',
+      price: 150,
+    },
+    {
+      id: 3,
+      name: 'Portable Bluetooth Speaker',
+      description: 'Compact and powerful Bluetooth speaker perfect for outdoor adventures.',
+      price: 200,
+    },
+  ]);
+}
