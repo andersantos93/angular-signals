c692d6373f3fc4595ca94426f6253a7c1b5e86bc
Anderson Santos <anderson.chenko1993@gmail.com>
2026-08-16 13:28:59 -0300

    feat: implement filter products based on name and description

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,15 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { ProductCard } from '../product-card/product-card';
 import { Product } from '../product';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard, MatIconModule],
+  imports: [ProductCard, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
 export class ProductsGrid {
+  protected readonly searchTerm = signal('');
+
   protected readonly products = signal<Product[]>([
     {
       id: 1,
@@ -34,4 +40,19 @@ export class ProductsGrid {
       originalPrice: 99.99,
     },
   ]);
+
+  protected readonly filteredProducts = computed(() => {
+    const term = this.searchTerm().toLocaleLowerCase().trim();
+    if (!term) return this.products();
+
+    return this.products().filter(
+      (product) =>
+        product.name.toLocaleLowerCase().includes(term) ||
+        product.description.toLocaleLowerCase().includes(term),
+    );
+  });
+
+  protected clearSearch(): void {
+    this.searchTerm.set('');
+  }
 }
